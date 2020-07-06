a223557b960cdc022d016e566ce41f4eb559781f
abdulsubhan2 <abdulsubhan041@gmail.com>
2020-07-06 18:34:48 +0530

    list components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component{
   constructor(){
     super();
 
     this.state = {
-      students:[
-        {
-          name: 'sample1',
-          id: 1
-        },
-        {
-          name:'sample2',
-          id: 2
-        },
-        {
-          name:'sample3',
-          id:3
-        }]
+      students:[]
     };
+  }
+  componentDidMount(){
+    fetch( 'https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({students: users}));
   }
   render(){
     return(
           <div className="App">
-            {
-              this.state.students.map(student => (<h1 key={student.id}>{student.name }</h1>))
-            }
+            <h1>Student List</h1>
+            <CardList students= {this.state.students}/>
           </div>
         );
       }
