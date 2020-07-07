6d673ff956e14c281adf5eef8694a24b266d835d
abdulsubhan2 <abdulsubhan041@gmail.com>
2020-07-07 12:42:11 +0530

    Added Search Bar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component{
   constructor(){
     super();
 
     this.state = {
-      students:[]
+      students:[],
+      searchField:''
     };
   }
   componentDidMount(){
@@ -17,10 +19,19 @@ class App extends Component{
     .then(users => this.setState({students: users}));
   }
   render(){
+    const {students, searchField} =this.state;
+    const filteredStudents = students.filter(student =>
+      student.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return(
           <div className="App">
+          
             <h1>Student List</h1>
-            <CardList students= {this.state.students}/>
+            <SearchBox 
+            placeholder = "search students"
+            handleChange = {e => this.setState({searchField : e.target.value})}
+            />
+            <CardList students= {filteredStudents}/>
           </div>
         );
       }
